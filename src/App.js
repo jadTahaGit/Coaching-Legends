e60c5b156574448bd3d2ed2d046bf06d2067e651
Jad Taha <jadtaha@MBP-von-Jad.fritz.box>
2022-01-29 23:31:08 +0100

    Started with 2nd Page version 08.00.00

--- src/App.js
+++ src/App.js
@@ -8,13 +8,27 @@ import BecomeACoach from "./components/BecomeACoach";
 import HappyStart from "./components/HappyStart";
 import Footer from "./components/Footer";
 import BAC__HeroSection from "./components/BAC__HeroSection";
+import CoachBenefits from "./components/CoachBenefits";
+import TeachStudents from "./components/TeachStudents";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <BAC__HeroSection />
+      <HeroSection />
+      <CoachingsOverview />
+      <Benefits />
+      <HowWeWork />
+      <BecomeACoach />
+      <HappyStart />
       <Footer></Footer>
+
+      <Header></Header>
+      <BAC__HeroSection />
+      <CoachBenefits></CoachBenefits>
+      <TeachStudents />
+
+      <Footer />
     </div>
   );
 }
---
+++ src/components/CoachBenefits.js
@@ -0,0 +1,39 @@
+import "./CoachBenefits.scss";
+const coachBenefits = () => {
+  return (
+    <div className="coachBenefits">
+      <div className="benefit__column">
+        <div className="circle"></div>
+        <div className="text-box">
+          <h1>Set your own rate</h1>
+          <p>
+            Choose your hourly rate and change it anytime. On average, English
+            tutors charge $15-25 per hour.
+          </p>
+        </div>
+      </div>
+      <div className="benefit__column">
+        <div className="circle"></div>
+        <div className="text-box">
+          <h1>Set your own rate</h1>
+          <p>
+            Choose your hourly rate and change it anytime. On average, English
+            tutors charge $15-25 per hour.
+          </p>
+        </div>
+      </div>
+      <div className="benefit__column">
+        <div className="circle"></div>
+        <div className="text-box">
+          <h1>Set your own rate</h1>
+          <p>
+            Choose your hourly rate and change it anytime. On average, English
+            tutors charge $15-25 per hour.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default coachBenefits;
