e3ecb63ba50cf39cde5612b22b7ee7244f929f4a
Ali-Allouch <ali.hallouch@gmail.com>
2022-05-28 20:13:52 +0300

    Update chat page design -- version 11.59.01

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -17,9 +17,7 @@ const Chat = () => {
         <div className="headOf__sidebar">
           <img src={woman} alt="" className="profile__photo" />
           <div className="right__options">
-            <div className="status">O</div>
-            <div className="newChat">N</div>
-            <div className="options">:</div>
+            <input type="search" className="search__for__users" placeholder="Search..." />
           </div>
         </div>
         <div className="mainOf__sidebar">
@@ -129,25 +127,89 @@ const Chat = () => {
           <img src={verified} alt="" className="profile__photo" />
           <div className="textbox">
             <div className="contactName">Pedro</div>
+            <div className="contact__name__info">
+              <div className="lastSeen">
+                <span>Last seen: 17:42</span>
+              </div>
+              <div className="localTime">
+                <span>Local time: 18:30</span>
+              </div>
+            </div>
           </div>
         </div>
         <div className="bodyOF__mainChat">
-          <div className="message__box sent">Hey Man how are you doing? </div>
-          <div className="message__box sent">I am Jad</div>
-          <div className="message__box sent">.</div>
-          <div className="message__box received">Hey</div>
-          <div className="message__box received">Ready for today's session?.</div>
-          <div className="message__box sent">Oh yeah!</div>
-          <div className="message__box sent">I am so excited</div>
-          <div className="message__box received">So let's start</div>
-          <div className="message__box sent">Hey Man how are you doing? </div>
-          <div className="message__box sent">Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</div>
-          <div className="message__box sent">.</div>
-          <div className="message__box received">Hey</div>
-          <div className="message__box received">Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</div>
-          <div className="message__box sent">Oh yeah!</div>
-          <div className="message__box sent">I am so excited</div>
-          <div className="message__box received">So let's start</div>
+          <div className="message__box__container reverse">
+            <div className="profile__pic__content">
+              <img src={woman} alt="" className="profile__photo" />
+            </div>
+            <div className="message__box__content">
+              <div className="message__box sent">Hey Man how are you doing? </div>
+              <div className="message__box sent">I am Jad</div>
+              <div className="message__box sent">.</div>
+            </div>
+          </div>
+          <div className="message__box__container">
+            <div className="profile__pic__content">
+              <img src={verified} alt="" className="profile__photo" />
+            </div>
+            <div className="message__box__content">
+              <div className="message__box received">Hey</div>
+              <div className="message__box received">Ready for today's session?.</div>
+            </div>
+          </div>
+          <div className="message__box__container reverse">
+            <div className="profile__pic__content">
+              <img src={woman} alt="" className="profile__photo" />
+            </div>
+            <div className="message__box__content">
+              <div className="message__box sent">Oh yeah!</div>
+              <div className="message__box sent">I am so excited</div>
+            </div>
+          </div>
+          <div className="message__box__container">
+            <div className="profile__pic__content">
+              <img src={verified} alt="" className="profile__photo" />
+            </div>
+            <div className="message__box__content">
+              <div className="message__box received">So let's start</div>
+            </div>
+          </div>
+          <div className="message__box__container reverse">
+            <div className="profile__pic__content">
+              <img src={woman} alt="" className="profile__photo" />
+            </div>
+            <div className="message__box__content">
+              <div className="message__box sent">Hey Man how are you doing? </div>
+              <div className="message__box sent">Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</div>
+              <div className="message__box sent">.</div>
+            </div>
+          </div>
+          <div className="message__box__container">
+            <div className="profile__pic__content">
+              <img src={verified} alt="" className="profile__photo" />
+            </div>
+            <div className="message__box__content">
+              <div className="message__box received">Hey</div>
+              <div className="message__box received">Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</div>
+            </div>
+          </div>
+          <div className="message__box__container reverse">
+            <div className="profile__pic__content">
+              <img src={woman} alt="" className="profile__photo" />
+            </div>
+            <div className="message__box__content">
+              <div className="message__box sent">Oh yeah!</div>
+              <div className="message__box sent">I am so excited</div>
+            </div>
+          </div>
+          <div className="message__box__container">
+            <div className="profile__pic__content">
+              <img src={verified} alt="" className="profile__photo" />
+            </div>
+            <div className="message__box__content">
+              <div className="message__box received">So let's start</div>
+            </div>
+          </div>
         </div>
         <div className="footerOf__mainChat">
           <div className="form-group">
