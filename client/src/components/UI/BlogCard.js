95f0a8487f2e88409d4dab19a985e0d48e07b16e
Ali-Allouch <ali.hallouch@gmail.com>
2022-06-19 09:01:34 +0300

    blogs page and component -- version 11.82.05
    moving the blogCard component to the UI folder, adjust the blogCard component to take props.

--- client/src/components/UI/BlogCard.js
+++ client/src/components/UI/BlogCard.js
@@ -1,27 +1,25 @@
 import React from "react";
-import "./blogs.scss";
-import bike from "./../../assets/blog/bike.jpeg";
 
-const BlogCard = () => {
+const BlogCard = (props) => {
     return (
         <div className="blog__card">
             <div className="card__header">
-                <img src={bike} alt="Blog" className="card__image" />
+                <img src={props.image} alt="Blog" className="card__image" />
             </div>
             <div className="card__body">
                 <div className="card__title">
-                    <h2>Why Cycling Improves My Thinking</h2>
+                    <h2>{props.title}</h2>
                 </div>
                 <div className="card__paragraph">
-                    I get clarity, and a big picture view, when i'm on two wheels. I'd return home knowing exactly what to do.
+                    {props.description}
                 </div>
             </div>
             <div className="card__footer">
                 <div className="card__date">
-                    Jun 12, 2022
+                    {props.date}
                 </div>
                 <div className="card__read__time">
-                    8 min read
+                    {props.readTime}
                 </div>
             </div>
         </div>
--- client/src/components/blog/blogs.js
+++ client/src/components/blog/blogs.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./blogs.scss";
-import BlogCard from "./BlogCard";
+import BlogCard from "../UI/BlogCard";
+import bike from "../../assets/blog/bike.jpeg";
 
 const Blogs = () => {
     return (
@@ -10,19 +11,73 @@ const Blogs = () => {
                     <h1 className="page__title">The Coachync Blog</h1>
                 </div>
                 <div className="blogs__row">
-                    <BlogCard />
-                    <BlogCard />
-                    <BlogCard />
+                    <BlogCard
+                        title={"Why Cycling Improves My Thinking"}
+                        description={"I get clarity, and a big picture view, when i'm on two wheels. I'd return home knowing exactly what to do."}
+                        date={"Jun 12, 2022"}
+                        readTime={"8 min read"}
+                        image={bike}
+                    />
+                    <BlogCard
+                        title={"Why Cycling Improves My Thinking"}
+                        description={"I get clarity, and a big picture view, when i'm on two wheels. I'd return home knowing exactly what to do."}
+                        date={"Jun 12, 2022"}
+                        readTime={"8 min read"}
+                        image={bike}
+                    />
+                    <BlogCard
+                        title={"Why Cycling Improves My Thinking"}
+                        description={"I get clarity, and a big picture view, when i'm on two wheels. I'd return home knowing exactly what to do."}
+                        date={"Jun 12, 2022"}
+                        readTime={"8 min read"}
+                        image={bike}
+                    />
                 </div>
                 <div className="blogs__row">
-                    <BlogCard />
-                    <BlogCard />
-                    <BlogCard />
+                    <BlogCard
+                        title={"Why Cycling Improves My Thinking"}
+                        description={"I get clarity, and a big picture view, when i'm on two wheels. I'd return home knowing exactly what to do."}
+                        date={"Jun 12, 2022"}
+                        readTime={"8 min read"}
+                        image={bike}
+                    />
+                    <BlogCard
+                        title={"Why Cycling Improves My Thinking"}
+                        description={"I get clarity, and a big picture view, when i'm on two wheels. I'd return home knowing exactly what to do."}
+                        date={"Jun 12, 2022"}
+                        readTime={"8 min read"}
+                        image={bike}
+                    />
+                    <BlogCard
+                        title={"Why Cycling Improves My Thinking"}
+                        description={"I get clarity, and a big picture view, when i'm on two wheels. I'd return home knowing exactly what to do."}
+                        date={"Jun 12, 2022"}
+                        readTime={"8 min read"}
+                        image={bike}
+                    />
                 </div>
                 <div className="blogs__row">
-                    <BlogCard />
-                    <BlogCard />
-                    <BlogCard />
+                    <BlogCard
+                        title={"Why Cycling Improves My Thinking"}
+                        description={"I get clarity, and a big picture view, when i'm on two wheels. I'd return home knowing exactly what to do."}
+                        date={"Jun 12, 2022"}
+                        readTime={"8 min read"}
+                        image={bike}
+                    />
+                    <BlogCard
+                        title={"Why Cycling Improves My Thinking"}
+                        description={"I get clarity, and a big picture view, when i'm on two wheels. I'd return home knowing exactly what to do."}
+                        date={"Jun 12, 2022"}
+                        readTime={"8 min read"}
+                        image={bike}
+                    />
+                    <BlogCard
+                        title={"Why Cycling Improves My Thinking"}
+                        description={"I get clarity, and a big picture view, when i'm on two wheels. I'd return home knowing exactly what to do."}
+                        date={"Jun 12, 2022"}
+                        readTime={"8 min read"}
+                        image={bike}
+                    />
                 </div>
             </div>
         </div>
