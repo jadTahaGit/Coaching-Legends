7ca24a104665bda10992adba1fad40ff5124eb01
PARAG NUKTE <paragnuktek@gmail.com>
2022-07-04 23:09:10 +0530

    Added main section under work

--- src/components/profile/NewProfile.js
+++ src/components/profile/NewProfile.js
@@ -1,16 +1,27 @@
-import './NewProfile.scss';
-import profilePhoto from '../../assets/coachProfile/jad.jpeg';
-import Star from '../../assets/service/star.svg';
-import EditIcon from '@mui/icons-material/Edit';
-import { useState } from 'react';
-import UpdateUserInfoForm from '../form/UpdateUserInfoForm';
+import "./NewProfile.scss";
+import profilePhoto from "../../assets/coachProfile/jad.jpeg";
+import Star from "../../assets/service/star.svg";
+import EditIcon from "@mui/icons-material/Edit";
+import { useState } from "react";
+import UpdateUserInfoForm from "../form/UpdateUserInfoForm";
 
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import PersonIcon from '@mui/icons-material/Person';
-import GoogleIcon from '@mui/icons-material/Google';
-import FacebookIcon from '@mui/icons-material/Facebook';
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import PersonIcon from "@mui/icons-material/Person";
+import GoogleIcon from "@mui/icons-material/Google";
+import FacebookIcon from "@mui/icons-material/Facebook";
+
+import america from "../../assets/services/america.svg";
+import heart from "../../assets/services/heart.svg";
+import grayHeart from "../../assets/services/grayHeart.svg";
+import woman from "../../assets/services/woman.jpg";
+import star from "../../assets/services/star.svg";
+import hamburger from "../../assets/services/hamburger.svg";
+import HomeBody from "./Services_cards";
 
 const CoachProfile = () => {
+  const goToProfileScreen = () => {
+    window.location.href = "/service";
+  };
   return (
     <div className="newCoachProfile">
       <div className="leftSidebar">
@@ -31,7 +42,7 @@ const CoachProfile = () => {
                   <img src={Star} alt="star" />
                 </div>
                 <span className="avg_rating">5</span>
-                <span className="num_of_orders">({'104'})</span>
+                <span className="num_of_orders">({"104"})</span>
               </div>
             </div>
             <hr className="hrGray" />
@@ -42,10 +53,10 @@ const CoachProfile = () => {
           <div className="middleSection">
             <div className="flex country">
               <LocationOnIcon /> Germany
-            </div>{' '}
+            </div>{" "}
             <div className="flex memberSince">
               <PersonIcon /> Feb 2022
-            </div>{' '}
+            </div>{" "}
           </div>
         </div>
         <div className="user2ndInfoBox">
@@ -59,7 +70,7 @@ const CoachProfile = () => {
               sit amet consectetur adipisicing elit. Necessitatibus quisquam
               inventore tempora! Amet eius quasi perferendis fugiat eligendi
               velit ea maxime pariatur iusto. Illum quasi maiores architecto sit
-              laboriosam sunt.{' '}
+              laboriosam sunt.{" "}
             </p>
           </div>
           <div className="languages">
@@ -87,7 +98,7 @@ const CoachProfile = () => {
             <div className="account">
               <GoogleIcon></GoogleIcon>
               <span>Google</span>
-            </div>{' '}
+            </div>{" "}
             <div className="account">
               <FacebookIcon></FacebookIcon>
               <span>Facebook</span>
@@ -107,7 +118,7 @@ const CoachProfile = () => {
               <p className="info">
                 Faculty of philosophy, University of Nis, Serbia, Graduated 2017
               </p>
-            </div>{' '}
+            </div>{" "}
             <div className="infoBox">
               <p className="infoTitle">B.Sc. - Psychology</p>
               <p className="info">
@@ -126,16 +137,70 @@ const CoachProfile = () => {
         </div>
       </div>
       <div className="main">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
-        pariatur aliquam vel deserunt at libero laboriosam? Omnis error qui
-        architecto! Cupiditate inventore minus ullam perferendis expedita iure
-        saepe assumenda magnam? Lorem ipsum dolor sit amet consectetur,
-        adipisicing elit. Exercitationem distinctio aperiam velit quisquam
-        voluptatem animi omnis doloribus reiciendis deserunt sint? Quasi cum
-        soluta earum maiores iure voluptas dolorem sit aut? Lorem ipsum dolor
-        sit amet, consectetur adipisicing elit. Nesciunt maxime obcaecati cumque
-        voluptatem dolorum officiis odit consequuntur iste excepturi dolore ipsa
-        et, odio magnam in sapiente repellat alias quos ullam?
+        <div>
+        <p className="title"><strong>pedro10's Gigs</strong></p>
+         <div className = "cards_wrapper"> <HomeBody />
+          <HomeBody />
+          <HomeBody />
+          <HomeBody />
+         </div>
+          
+        </div>
+
+        {/* <div
+      onClick={goToProfileScreen}
+      className="HomeBody">
+      <div className="">
+        <div className="main">
+          <p className="numberOf__results">
+            <span>13,150</span> English teachers available
+          </p>
+
+          <div className="gig__collection">
+            <div className="gig">
+              <div className="header__box">
+                <div className="flag">
+                  <img src={america} alt="" />
+                </div>
+                <div className="addToFav">
+                  <img src={heart} alt="" />
+                </div>
+              </div>
+              <div className="middle__box">
+                <div className="userInfo">
+                  <img src={woman} alt="" />
+                  <div className="textBox">
+                    <p className="fullName">Pedro Fed</p>
+                    <p className="rank">Rank 1</p>
+                  </div>
+                </div>
+                <div className="catagory">
+                  <p>Life Coauch to help you with you life´s problems</p>
+                </div>
+                <div className="stars">
+                  <img src={star} alt="" />
+                  <p>4.1</p>
+                  <p className="gray">(106)</p>
+                </div>
+              </div>
+              <div className="footer__box">
+                <div className="images_wrapper">
+                  <img src={hamburger} alt="hamburger Icon" />
+                  <img src={grayHeart} alt="heart Icon" />
+                </div>
+                <div className="textBox">
+                  <p>Starting at </p>
+                  <p className="price">
+                    US$<span>160</span>{" "}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="left__sidebar"></div>
+      </div>
+    </div> */}
       </div>
     </div>
   );
