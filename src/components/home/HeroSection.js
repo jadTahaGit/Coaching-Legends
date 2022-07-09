ee8606e725ddc4de57fe92bb83ccf858d45f3957
Jad Taha <jadtaha.de@gmail.com>
2022-07-09 21:57:36 +0200

    HeroSection Responsiv  --version 11.83.40

--- src/components/home/HeroSection.js
+++ src/components/home/HeroSection.js
@@ -1,22 +1,24 @@
-import "./HeroSection.scss";
-import woman from "./../../assets/home/woman_noBG.png";
+import './HeroSection.scss';
+import woman from './../../assets/home/woman_noBG.png';
 
 const HeroSection = () => {
   return (
     <div className="HeroSection">
-      <img src={woman} alt="woman foto" className="heroImage" />
-      <h1 className="hero__heading">
-        Find Your <span className="Cookie">Coach,</span>
-        <br />
-        Find your Match
-      </h1>
-      <div className="search__wrapper">
-        <input
-          type="text"
-          className="search__input"
-          placeholder="Search for Coaches"
-        />
-        <button className="search__btn">Search</button>
+      <div className="relativeBox">
+        <img src={woman} alt="woman foto" className="heroImage" />
+        <h1 className="hero__heading">
+          Find Your <span className="Cookie">Coach,</span>
+          <br />
+          Find your Match
+        </h1>
+        <div className="search__wrapper">
+          <input
+            type="text"
+            className="search__input"
+            placeholder="Search for Coaches"
+          />
+          <button className="search__btn">Search</button>
+        </div>
       </div>
     </div>
   );
