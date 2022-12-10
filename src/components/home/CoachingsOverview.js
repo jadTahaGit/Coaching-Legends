d2269c1ca0bef37bb4744ed8b3f9c0996e5c0c4f
Ali-Allouch <ali.hallouch@gmail.com>
2022-12-10 09:55:23 +0200

    home page responsiveness --version 12.30.59

--- src/components/home/CoachingsOverview.js
+++ src/components/home/CoachingsOverview.js
@@ -13,101 +13,101 @@ const CoachingsOverview = () => {
   return (
     <div className="CoachingsOverview">
       <div className="CO__container">
-        <div className="catagory">
-          <div className="catagory__icon__wrapper">
+        <div className="category">
+          <div className="category__icon__wrapper">
             <img src={person} alt="" />
           </div>
-          <div className="catagory__Text__box">
-            <h3 className="catagory__title">Life Coaching</h3>
-            <p className="catagory__numOfCoaches">
+          <div className="category__Text__box">
+            <h3 className="category__title">Life Coaching</h3>
+            <p className="category__numOfCoaches">
               <span>30000</span> Coaches
             </p>
           </div>
         </div>
-        <div className="catagory">
-          <div className="catagory__icon__wrapper">
+        <div className="category">
+          <div className="category__icon__wrapper">
             <img src={bag} alt="" />
           </div>
-          <div className="catagory__Text__box">
-            <h3 className="catagory__title">Business Coaching</h3>
-            <p className="catagory__numOfCoaches">
+          <div className="category__Text__box">
+            <h3 className="category__title">Business Coaching</h3>
+            <p className="category__numOfCoaches">
               <span>30000</span> Coaches
             </p>
           </div>
         </div>
-        <div className="catagory">
-          <div className="catagory__icon__wrapper">
+        <div className="category">
+          <div className="category__icon__wrapper">
             <img src={calender} alt="" />
           </div>
-          <div className="catagory__Text__box">
-            <h3 className="catagory__title">Accountability Coaching</h3>
-            <p className="catagory__numOfCoaches">
+          <div className="category__Text__box">
+            <h3 className="category__title">Accountability Coaching</h3>
+            <p className="category__numOfCoaches">
               <span>30000</span> Coaches
             </p>
           </div>
         </div>
-        <div className="catagory">
-          <div className="catagory__icon__wrapper">
+        <div className="category">
+          <div className="category__icon__wrapper">
             <img src={plus} alt="" />
           </div>
-          <div className="catagory__Text__box">
-            <h3 className="catagory__title">Health and Nutrition Coach</h3>
-            <p className="catagory__numOfCoaches">
+          <div className="category__Text__box">
+            <h3 className="category__title">Health and Nutrition Coach</h3>
+            <p className="category__numOfCoaches">
               <span>30000</span> Coaches
             </p>
           </div>
         </div>
-        <div className="catagory">
-          <div className="catagory__icon__wrapper">
+        <div className="category">
+          <div className="category__icon__wrapper">
             <img src={ball} alt="" />
           </div>
-          <div className="catagory__Text__box">
-            <h3 className="catagory__title">Sport Coach</h3>
-            <p className="catagory__numOfCoaches">
+          <div className="category__Text__box">
+            <h3 className="category__title">Sport Coach</h3>
+            <p className="category__numOfCoaches">
               <span>30000</span> Coaches
             </p>
           </div>
         </div>
-        <div className="catagory">
-          <div className="catagory__icon__wrapper">
+        <div className="category">
+          <div className="category__icon__wrapper">
             <img src={love} alt="" />
           </div>
-          <div className="catagory__Text__box">
-            <h3 className="catagory__title">Dating Coach</h3>
-            <p className="catagory__numOfCoaches">
+          <div className="category__Text__box">
+            <h3 className="category__title">Dating Coach</h3>
+            <p className="category__numOfCoaches">
               <span>30000</span> Coaches
             </p>
           </div>
         </div>
-        <div className="catagory">
-          <div className="catagory__icon__wrapper">
+        <div className="category">
+          <div className="category__icon__wrapper">
             <img src={dollar} alt="" />
           </div>
-          <div className="catagory__Text__box">
-            <h3 className="catagory__title">Financial Coach</h3>
-            <p className="catagory__numOfCoaches">
+          <div className="category__Text__box">
+            <h3 className="category__title">Financial Coach</h3>
+            <p className="category__numOfCoaches">
               <span>30000</span> Coaches
             </p>
           </div>
         </div>
-        <div className="catagory">
-          <div className="catagory__icon__wrapper">
+        <div className="category">
+          <div className="category__icon__wrapper">
             <img src={graduate} alt="" />
           </div>
-          <div className="catagory__Text__box">
-            <h3 className="catagory__title">Career Coach</h3>
-            <p className="catagory__numOfCoaches">
+          <div className="category__Text__box">
+            <h3 className="category__title">Career Coach</h3>
+            <p className="category__numOfCoaches">
               <span>30000</span> Coaches
             </p>
           </div>
         </div>
-        <div className="catagory">
-          <div className="catagory__icon__wrapper">
+        <div className="category">
+          <div className="category__icon__wrapper">
             <img src={people} alt="" />
           </div>
-          <div className="catagory__Text__box">
-            <h3 className="catagory__title">Coaching for Teams</h3>
-            <p className="catagory__numOfCoaches">
+          <div className="category__Text__box">
+            <h3 className="category__title">Coaching for Teams</h3>
+            <p className="category__numOfCoaches">
               <span>30000</span> Coaches
             </p>
           </div>
