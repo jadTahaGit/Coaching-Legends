6f1b533f4d92e46c746b8b874fbf416799c680b3
jadTahaGit <jadtaha.de@gmail.com>
2023-01-07 14:32:30 +0100

    AboutUs Responsive --version 12.30.73

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,53 +3,74 @@ import './About.scss';
 const AboutUs = () => {
   return (
     <div className="AboutUS">
-      <p>
-        {' '}
-        Welcome to Find Your Coach, your ultimate destination for finding the
-        perfect coach tailored to your unique needs and goals. We believe that
-        everyone has the potential to unlock their full potential, and our
-        mission is to connect individuals like you with experienced and
-        qualified coaches who can guide you on your journey to success.
-      </p>
-      <p>
-        At Find Your Coach, we understand that finding the right coach can be a
-        daunting task. With so many different coaching specialties and
-        approaches available, it can be challenging to find a match that aligns
-        with your specific requirements. That's where we come in. We have
-        curated a diverse network of talented coaches from various fields and
-        expertise, ensuring that there's a perfect match for everyone.
-      </p>
-      <p>
-        Whether you're looking for a life coach, career coach, wellness coach,
-        relationship coach, or any other type of coach, our platform simplifies
-        the process of finding and connecting with them. We provide a
-        user-friendly interface where you can browse through profiles, read
-        about each coach's background and specialties, and even view
-        testimonials from their satisfied clients. With our comprehensive search
-        filters, you can narrow down your options based on location, coaching
-        style, availability, and more.
-      </p>
-      <p>
-        Finding your coach is just the beginning of your transformative journey.
-        Our platform fosters a supportive and empowering environment for both
-        coaches and clients. We encourage open communication, trust, and
-        collaboration between coaches and individuals seeking their guidance.
-        Our aim is to create meaningful connections that drive personal growth
-        and help you achieve your goals.
-      </p>
-      <p>
-        Whether you're seeking guidance in personal development, career
-        advancement, health and well-being, or any other area of your life, Find
-        Your Coach is here to assist you every step of the way. Discover the
-        perfect coach for you and embark on a transformative partnership that
-        will propel you towards success and fulfillment.
-      </p>
-      <p>
-        {' '}
-        Join Find Your Coach today and embark on a journey of self-discovery,
-        growth, and achievement. Let us help you find your match and unlock your
-        true potential.
-      </p>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <p>
+              Welcome to Find Your Coach, your ultimate destination for finding
+              the perfect coach tailored to your unique needs and goals. We
+              believe that everyone has the potential to unlock their full
+              potential, and our mission is to connect individuals like you with
+              experienced and qualified coaches who can guide you on your
+              journey to success.
+            </p>
+
+            <p>
+              At Find Your Coach, we understand that finding the right coach can
+              be a daunting task. With so many different coaching specialties
+              and approaches available, it can be challenging to find a match
+              that aligns with your specific requirements. That's where we come
+              in. We have curated a diverse network of talented coaches from
+              various fields and expertise, ensuring that there's a perfect
+              match for everyone.
+            </p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col">
+            <p>
+              Whether you're looking for a life coach, career coach, wellness
+              coach, relationship coach, or any other type of coach, our
+              platform simplifies the process of finding and connecting with
+              them. We provide a user-friendly interface where you can browse
+              through profiles, read about each coach's background and
+              specialties, and even view testimonials from their satisfied
+              clients. With our comprehensive search filters, you can narrow
+              down your options based on location, coaching style, availability,
+              and more.
+            </p>
+
+            <p>
+              Finding your coach is just the beginning of your transformative
+              journey. Our platform fosters a supportive and empowering
+              environment for both coaches and clients. We encourage open
+              communication, trust, and collaboration between coaches and
+              individuals seeking their guidance. Our aim is to create
+              meaningful connections that drive personal growth and help you
+              achieve your goals.
+            </p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col">
+            <p>
+              Whether you're seeking guidance in personal development, career
+              advancement, health and well-being, or any other area of your
+              life, Find Your Coach is here to assist you every step of the way.
+              Discover the perfect coach for you and embark on a transformative
+              partnership that will propel you towards success and fulfillment.
+            </p>
+
+            <p>
+              Join Find Your Coach today and embark on a journey of
+              self-discovery, growth, and achievement. Let us help you find your
+              match and unlock your true potential.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
